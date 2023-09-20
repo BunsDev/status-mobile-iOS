a786e1d6a7bf3abb8ff89a4cd235d3eee329b661
Ulises M <ulises.ssb@hotmail.com>
2023-09-20 09:23:20 +0900

    WIP: adding gestures to control amount

--- src/quo2/components/wallet/network_routing/view.cljs
+++ src/quo2/components/wallet/network_routing/view.cljs
@@ -2,23 +2,46 @@
   (:require
    [quo2.foundations.colors :as colors]
    [react-native.core :as rn]
+   [react-native.gesture :as gesture]
    [react-native.reanimated :as reanimated]
+   [oops.core :as oops]
    [reagent.core :as reagent]))
 
-(defn slider []
+(defn slider
   []
-  [rn/view {:style {:width           8
-                    :justify-content :center
-                    :align-items     :center
-                    :right           -4}}
-   [reanimated/view
-    {:style {:background-color colors/white
-             :height           32
-             :width            4
-             :border-radius    4}}]])
+  (let [appearing-opacity (reanimated/use-shared-value 0)
+        width             (reanimated/use-shared-value 4)
+        height            (reanimated/use-shared-value 32)
+        ]
+
+    (rn/use-effect
+     (fn []
+       (reanimated/animate appearing-opacity 1 300)
+       ;; TODO: think how to disappear the component animated
+       #(reanimated/animate appearing-opacity 0 300)))
+
+    [rn/touchable-without-feedback {:on-press-in  (fn []
+                                                    (reanimated/animate width 8 300)
+                                                    (reanimated/animate height 40 300))
+                                    :on-press-out (fn []
+                                                    (reanimated/animate width 4 300)
+                                                    (reanimated/animate height 32 300))}
+     [rn/view {:style {:width           8
+                       :justify-content :center
+                       :align-items     :center
+                       :right           -4}}
+      [reanimated/view
+       {:style (reanimated/apply-animations-to-style
+                {:opacity appearing-opacity
+                 :height  height
+                 :width   width}
+                {:background-color colors/white
+                 :height           32
+                 :width            4
+                 :border-radius    4})}]]]))
 
 (defn network-bar []
-  (let [show-slider? (reagent/atom false)]
+  (let [show-slider?    (reagent/atom false)]
     (fn [{:keys [max-width selected-width]}]
       [reanimated/view {:style {:width            (if @show-slider?
                                                     max-width
@@ -27,13 +50,20 @@
                                 :flex-direction   :row
                                 :justify-content  :space-between}}
 
+
        [rn/touchable-without-feedback {:on-press #(swap! show-slider? not)}
         [reanimated/view {:style {:width            selected-width
                                   :flex-direction   :row
                                   :justify-content  :flex-end
                                   :background-color "#758EEB"}}
          (when @show-slider?
-           [slider])]]
+           [gesture/gesture-detector
+            {:gesture (-> (gesture/gesture-pan)
+                          (gesture/enabled @show-slider?)
+                          (gesture/on-update (fn [event]
+                                               (prn (oops/oget event "x"))
+                                               nil)))}
+            [:f> slider]])]]
 
        (when @show-slider?
          [reanimated/view
